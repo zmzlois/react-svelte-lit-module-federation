84c7565209347d8eb44cd539e073baf09a97450a
zmzlois <lois.z@comcord.vision>
2024-06-11 17:31:52 +0100

    port

---
+++ remote-svelte/rsbuild.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginSvelte } from '@rsbuild/plugin-svelte';
+
+export default defineConfig({
+  plugins: [pluginSvelte()],
+  server: {
+    port: 3003
+  }
+});
